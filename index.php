455c888629e67e70f027d2773d75b743d0bbfe8c
unknown <aogara@hawk.iit.edu>
2015-04-05 23:54:17 -0500

    INCOMPLETE CRUD FUNCTIONALITY

--- index.php
+++ index.php
@@ -24,7 +24,6 @@ if (isset($_SESSION["trains"])){
 <div ng-app="wellSprings" class="hook" ng-controller="ListController" ng-show="checked">
     <?php include 'partials/trains.php';?>
 </div>
-
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.3.15/angular-route.js"></script>
